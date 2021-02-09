d6a755f87e90dbe7f549d8be1a56cc83cbfce0f8
Matt Maddux <mattmaddux@gmail.com>
2021-02-09 15:30:59 -0600

    Next

--- installHomebrew.sh
+++ installHomebrew.sh
@@ -5,3 +5,18 @@ URL_BREW='https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'
 echo -n '- Installing brew ... '
 echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
 if [ $? -eq 0 ]; then echo 'OK'; else echo 'NG'; fi
+
+
+
+#!/usr/bin/env bash
+# install.sh
+
+URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
+
+echo -n '- Installing brew ... '
+echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
+if [ $? -eq 0 ]; then echo 'OK'; else echo 'NG'; fi
+
+
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
